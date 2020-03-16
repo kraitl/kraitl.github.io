104cea53f4f477ae3117ab73d75ef000603dd235
kraitl <31151598+kraitl@users.noreply.github.com>
2020-03-16 14:18:04 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -10,10 +10,7 @@ window.Config = {
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur887147-9a39452a7ba3e5ad266762e4'
   ],
 
   // 是否显示监测站点的链接
@@ -28,7 +25,7 @@ window.Config = {
   Navi: [
     {
       text: 'Homepage',
-      url: 'https://status.org.cn/'
+      url: 'https://status.krail.org/'
     },
     {
       text: 'GitHub',
